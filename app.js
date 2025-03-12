e18bcdda3795fc5828c4299ad35d8e04595e0c0c
hongphuc <phuc21744@gmail.com>
2025-03-12 18:26:55 +0700

    Example page

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', { title: 'Home', message: 'Hello there!' });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on http://localhost:${port}`)
 })

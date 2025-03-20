d3b1e3f0fc7a956c3a071c12948b203c8557d7ca
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-03-20 10:13:27 +0700

    refactor address

--- models/addressModel.js
+++ models/addressModel.js
@@ -1,22 +1,15 @@
 import mongoose, { SchemaTypes } from "mongoose";
 import paginate from 'mongoose-paginate-v2';
 import aggregatePaginate from 'mongoose-aggregate-paginate-v2';
-import Student from "./studentModel";
 
 const AddressSchema = mongoose.Schema({
     _id: String,
-    student_id: {
-        type: String,
-        ref: Student
-    },
     house_number: String,
     street: String,
     ward: String, // Phường, xã
     district: String,
     city: String,
     country: String,
-    postal_code: String,
-    is_permanent: Boolean,
 });
 
 AddressSchema.plugin(paginate);

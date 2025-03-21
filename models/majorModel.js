f467e8d9dd3186d8f781ba2be66ec1613814a201
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-03-21 14:08:34 +0700

    Revert "Merge pull request #4 from phuong5514/logging_and_import"
    This reverts commit 7de310d12dac212ee6d0e720480a9fc5f407afeb, reversing
    changes made to 5cbf2301f567e0b1a9f4f56df1e7a8f5bcffbda6.

--- models/majorModel.js
+++ models/majorModel.js
@@ -1,39 +1,10 @@
 import mongoose from "mongoose";
 
-const majorSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        required: true
-    },
-    major_name: {
-        type: String,
-        required: true
-    }
+const MajorSchema = mongoose.Schema({
+  _id : String,
+  major_name : String
 });
 
-// Tạo danh sách ngành học mặc định nếu chưa có
-majorSchema.statics.createDefaultMajors = async function() {
-    const defaultMajors = [
-        { _id: "KHOATATM", major_name: "Tiếng Anh Thương mại" },
-        { _id: "KHOALUAT", major_name: "Luật" },
-        { _id: "KHOANHAT", major_name: "Tiếng Nhật" },
-        { _id: "KHOAPHAP", major_name: "Tiếng Pháp" }
-    ];
-
-    try {
-        const count = await this.countDocuments();
-        if (count === 0) {
-            await this.insertMany(defaultMajors);
-            console.log('Created default majors');
-        }
-    } catch (error) {
-        console.error('Error creating default majors:', error);
-    }
-};
-
-const Major = mongoose.model("Major", majorSchema);
-
-// Tạo dữ liệu mặc định khi khởi động
-Major.createDefaultMajors();
+const Major = mongoose.model("Major", MajorSchema, "majors");
 
 export default Major;
--- models/programModel.js
+++ models/programModel.js
@@ -1,39 +1,10 @@
 import mongoose from "mongoose";
 
 const ProgramSchema = mongoose.Schema({
-  _id: {
-    type: String,
-    required: true
-  },
-  program_name: {
-    type: String,
-    required: true
-  }
+  _id : String,
+  program_name : String
 });
 
-// Tạo danh sách chương trình học mặc định nếu chưa có
-ProgramSchema.statics.createDefaultPrograms = async function() {
-  const defaultPrograms = [
-    { _id: "CTDT", program_name: "Đại trà" },
-    { _id: "CTTN", program_name: "Tài năng" },
-    { _id: "CTTT", program_name: "Tiên tiến" },
-    { _id: "CTCLC", program_name: "Chất lượng cao" }
-  ];
-
-  try {
-    const count = await this.countDocuments();
-    if (count === 0) {
-      await this.insertMany(defaultPrograms);
-      console.log('Created default programs');
-    }
-  } catch (error) {
-    console.error('Error creating default programs:', error);
-  }
-};
-
 const Program = mongoose.model("Program", ProgramSchema, "programs");
-
-// Tạo dữ liệu mặc định khi khởi động
-Program.createDefaultPrograms();
 
 export default Program;

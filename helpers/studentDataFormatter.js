c0665dcfcfb08aa5252b762e3767c5b39eba0678
Nguyen Manh Phuong <manhphuong5514@gmail.com>
2025-03-27 19:02:47 +0700

    refactor fetchAndFormatStudents and preprocessStudent

--- helpers/studentDataFormatter.js
+++ helpers/studentDataFormatter.js
@@ -7,3 +7,40 @@ export const formatStudentsData = (students) => {
 }
 
 export default formatStudentsData;
+
+export const formatIdentificationDocument = (document, formatTypeFunction) => {
+  if (document) {
+    document.issue_date = dayjs(document.issue_date).format('YYYY-MM-DD');
+    document.expiry_date = dayjs(document.expiry_date).format('YYYY-MM-DD');
+    document.text = formatTypeFunction(document);
+  } else {
+    document = null;
+  }
+}
+
+export const formatAddress = (address) => {
+  if (!address) {
+    return "";
+  }
+  return Object.entries(address)
+    .filter(([key, value]) => (key === "city" || key === "country") && typeof value !== "undefined" && value)
+    .map(([key, value]) => value)
+    .join(", ");
+}
+
+export const formatIdentityCard = (identityCard) => {
+  if (!identityCard){
+    return "";
+  }
+
+  return identityCard._id;
+}
+
+export const formatPassport = (passport) => {
+  if (!passport){
+    return "";
+  }
+  return passport._id;
+}
+
+
